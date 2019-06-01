814a918e0f59c98e85346aa5c77ef78a3616e6d1
David <digital0david@gmail.com>
2019-06-01 13:30:26 -0400

    post

--- data/projectRouter.js
+++ data/projectRouter.js
@@ -5,4 +5,31 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name && !description) {
+    res.status(400).json({
+      message: "Please provide a name and a description for the project."
+    });
+  } else if (!description) {
+    res.status(400).json({
+      message: "Please provide a description for the project."
+    });
+  } else if (!name) {
+    res.status(400).json({
+      message: "Please provide a name for the project."
+    });
+  }
+
+  db.insert(req.body, ["id"])
+    .into("projects")
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
